daeafc7a23493964fa8e0eeeea213c179dc0d361
Ludwig Andersson <dv13lan@cs.umu.se>
2016-03-12 19:30:23 +0100

    Fixed warnings

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,8 +27,8 @@ class User < ActiveRecord::Base
 
   def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
-      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    if (login = conditions.delete(:login))
+      where(conditions).where(['lower(username) = :value OR lower(email) = :value', {:value => login.downcase }]).first
     else
       if conditions[:username].nil?
         where(conditions).first
