f1c099792593b3e238d9c44a141692096dd66f42
Ludwig Andersson <dv13lan@cs.umu.se>
2016-03-19 04:12:27 +0100

    Implemented navigation buttons and fixed some bugs

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,20 @@ class Post < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :comments
+
+  #
+  def self.get_total_number_of_posts (category_id)
+    i = 0
+    @posts = Post.all.where('category_id' => category_id)
+
+    @posts.each do |post|
+      i += 1
+
+      post.comments.each do
+        i += 1
+      end
+    end
+    return i
+  end
+
 end
