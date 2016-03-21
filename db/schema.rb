73eb01f679cbe7ae28c1fbb6decfff2d4d6c11ca
Ludwig Andersson <dv13lan@cs.umu.se>
2016-03-21 23:44:07 +0100

    Added news section

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160316225653) do
+ActiveRecord::Schema.define(version: 20160321014958) do
+
+  create_table "announcements", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "articles", force: :cascade do |t|
+    t.text     "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
