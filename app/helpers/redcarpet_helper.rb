b55df5558bbc2ffe6955a2b0bb8b4140df1ae10e
Ludwig Andersson <dv13lan@cs.umu.se>
2016-03-26 03:35:56 +0100

    Fixed posts styling with markdown

--- app/helpers/redcarpet_helper.rb
+++ app/helpers/redcarpet_helper.rb
@@ -5,7 +5,7 @@ module RedcarpetHelper
     def image(url, title, alt_text)
       # Make sure to return an image tag anyway ; this method
       # overrides the vanilla output.
-      %(<img src="#{url}" alt="#{alt_text}" title="#{title}>" class="img-responsive")
+      %(<img src="#{url}" alt="#{alt_text}" title="#{title}>" class="img-responsive post-image")
     end
 
   end
