bf23f453020157924a1cfddef70243a81c5868ce
Ludwig Andersson <dv13lan@cs.umu.se>
2016-03-26 02:51:39 +0100

    Added markdown formatting to Posts and to Articles.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,5 +15,14 @@ module ApplicationHelper
         flash_type.to_s
     end
   end
+
+  def markdown(text)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+                                       no_intra_emphasis: true,
+                                       fenced_code_blocks: true,
+                                       disable_indented_code_blocks: true)
+    return markdown.render(text).html_safe
+  end
+
   
 end
