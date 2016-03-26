07e4f69085b5a787a430e27b6eb9ae36d364c24f
Ludwig Andersson <dv13lan@cs.umu.se>
2016-03-26 03:25:35 +0100

    Added automatic scaling for images linked in markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,7 +17,7 @@ module ApplicationHelper
   end
 
   def markdown(text)
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+    markdown = Redcarpet::Markdown.new(RedcarpetHelper::HTMLBlockCode,
                                        no_intra_emphasis: true,
                                        fenced_code_blocks: true,
                                        disable_indented_code_blocks: true)
